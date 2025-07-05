4a2e67ba973670a431f2ad7152204c55654265a6
DeSaintJust <114399784+DeSaintJust@users.noreply.github.com>
2025-07-05 19:36:04 +1000

    Create gke.py

---
+++ cloud/gke.py
@@ -0,0 +1,28 @@
+# propositions describing conditions that are required for an attack to succeed
+propositions = {
+  "p1":"Adversary has administrative access to a Google Kubernetes Engine Cluster"
+  "p2":"Adversary does not have access"
+}
+
+if propositions["p2"]:
+  Diamond(propositions["p1"])
+
+# mitigations that negate specific propositions
+mitigations = {}
+
+if any(m for ms in mitigations.keys()):
+  Box(not p2)
+
+W = {
+  "w1":"Unauthenticated", 
+  "w2":"Administrative access to Google Kubernetes Engine Cluster"
+}
+
+R = [[W["w1"], W["w2"]]]
+
+v = {
+  W["w1"]: [propositions["p2"]],
+  W["w2"]: [propositions["p1"]]
+}
+
+model = [W, R, v]
